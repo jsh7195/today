dbf24d84033505826df6bf28041c42ee7a5ec8be
shji <jsh7195gg@gmail.com>
2021-09-04 19:39:28 +0900

    1st proto
    1st proto

--- src/components/module/LabelInput/LabelInput.tsx
+++ src/components/module/LabelInput/LabelInput.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
 interface Props {
-    loading?: boolean;
-    readOnly?: boolean
-    label: string;
-    value: string;
+  loading?: boolean;
+  readOnly?: boolean;
+  id: string;
+  label: string;
+  value: string;
+  setData: any;
 }
 
 const LabelInput = ({
-    loading = false,
-    readOnly,
-    label,
-    value
+  loading = false,
+  label,
+  value,
+  id,
+  setData,
 }: Props): React.ReactElement => {
-    return (
-        <div>
-            <label htmlFor={label}>
-                {label}
-            </label>
-            <input type="text" id={label} value={value} readOnly={readOnly}
-            />
-        </div>
-    )
-}
+  return (
+    <div>
+      <label htmlFor={id}>{label}</label>
+      <input
+        style={{ color : 'black'}}
+        type="text"
+        key={id}
+        name={id}
+        id={id}
+        value={value}
+        defaultValue="0"
+        onChange={(e) => {
+          setData(e.target.value);
+        }}
+      />
+    </div>
+  );
+};
 
 export default LabelInput;
--- src/components/module/Odin/DemageCalc.tsx
+++ src/components/module/Odin/DemageCalc.tsx
@@ -1,16 +1,81 @@
 import React, { useEffect, useState } from 'react';
+import LabelInput from '../LabelInput/LabelInput';
 import moment from 'moment';
 import _ from 'lodash';
 
 const DemageCalc = (): React.ReactElement => {
 
+    const [ power, setPower ] = useState("0");
+    const [ allpower, setAllPower ] = useState("0");
+    const [ skillPower, setSkillPower ] = useState("0");
+    const [ anyPower, setAnyPower ] = useState("0");
+    
+    const [ skillRate, setSkillRate ] = useState("0");
+
+    const [ baseDmg , setDmg ] = useState(0);
+    const [ skillDmg , setSkillDmg ] = useState(0);
+
+    const [ realHitDmg, setRealHitDmg ] = useState("0");
+
+    const [ correction, setCorrection ] = useState("0");
+
+    const percentCalc = (value:any) => {
+        const valueInt = Number(value);
+        const percentage = (valueInt * 0.01) + 1;
+       
+        return Number(percentage.toFixed(2));
+    }
+
+    const skillRateCalc = (value:any) => {
+        console.log(`${value} = > ${Number(value) / 100}`);
+        return Number(value) / 100;
+    }
+
+    const caclDmg = () => {
+        const calcBaseDmg = Number(power) * percentCalc(anyPower) * percentCalc(allpower);
+        setDmg(Number(calcBaseDmg.toFixed(2)));
+        const calcSkillDmg = calcBaseDmg * percentCalc(skillPower) * skillRateCalc(skillRate);
+        setSkillDmg(Number(calcSkillDmg.toFixed(2)));
+    }
+
 
     useEffect(() => {
+        caclDmg();
+    }, [power,allpower,skillPower,anyPower,skillRate])
 
-    }, [])
+    useEffect(() => {
+        
+        if(Number(realHitDmg) > Number(baseDmg)){
+            const diff = Number(realHitDmg) - Number(baseDmg);
+            const calcRate = (diff/Number(baseDmg))*100;
+            setCorrection(String(calcRate.toFixed(2)));
+        } else if(Number(realHitDmg) < Number(baseDmg)){
+            // 실제 때린 데미지가 작을 때
+            const diff = Number(baseDmg) - Number(realHitDmg);
+            const calcRate = (diff / Number(baseDmg)) * 100;
+            setCorrection(String("-"+calcRate.toFixed(2)));
+        }
+    }, [realHitDmg])
+
 
     return (
-        <div>test</div>
+        <div>
+          <LabelInput id="power" label="공격력 : " value={power} setData={setPower} />
+          <LabelInput id="allpower" label="근접,원거리,마법 데미지 % : " value={allpower} setData={setAllPower} /> 
+          <LabelInput id="anyPower" label="모든 데미지 % : " value={anyPower} setData={setAnyPower} /> 
+          <LabelInput id="skillPower" label="스킬 데미지 % : " value={skillPower} setData={setSkillPower} /> 
+
+          <LabelInput id="skillRate" label="스킬 계수 % : " value={skillRate} setData={setSkillRate} /> 
+          
+          <LabelInput id="baseDmg" label="계산한 평타 데미지 : " value={String(baseDmg)} setData={setDmg} /> 
+          <LabelInput id="skillDmg" label="계산한 스킬 데미지 : " value={String(skillDmg)} setData={setSkillDmg} /> 
+
+
+          <LabelInput id="realHitDmg" label="실제 몬스터 공격시 나오는 평타 데미지 : " value={realHitDmg} setData={setRealHitDmg} /> 
+
+          <LabelInput id="correction" label="내 공격력레벨과 몬스터레벨 보정치 % : " value={correction} setData={setCorrection} /> 
+
+        </div>
     )
 }
 
